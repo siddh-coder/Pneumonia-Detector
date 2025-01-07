5417fe9b2a2dc58826e11260899e4819264c3b3f
Siddharth Tripathi <siddharthtripathi3824@gmail.com>
2025-01-07 07:55:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def predict_ensemble(image):
     return class_labels[ensemble_prediction], predictions
 
 # Streamlit UI
-st.markdown("#🩺PneumaScan🩻")
-st.markdown("##🫁Pneumonia Detection App🩺", divider="rainbow")
+st.markdown("#PneumaScan🩻")
+st.markdown("##🫁Pneumonia Detection App🩺", divider="green")
 st.write("This app uses the x-ray image of the patient to detect whether the patient has Pneumonia or not. Upload an image to get the prediction!")
 
 # Function to validate whether the image is a chest X-ray
